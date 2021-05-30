1a1dec1bb7b40d17d43ccf5df799d2229b055e96
berny88 <bernard.bougeon@gmail.com>
2021-05-30 11:30:11 +0200

    sign rename

---
+++ front/src/views/sign.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+       <div class="columns col-gapless">
+         <div class="column col-xs-12">
+            <center>
+                <router-link to="/signin">
+                    <img class="bg-primary text-light docs-shape s-circle centered" src="../assets/img/login.jpg" alt="">
+                    <h2>Log in !</h2>
+                </router-link>
+            </center>
+        </div>
+        <div class="column col-xs-12">
+            <center>
+                <router-link to="/signon">
+                    <img class="bg-primary text-light docs-shape s-circle centered" src="../assets/img/signon.jpg" alt="">
+                    <h2>Sign Up !</h2>
+                </router-link>
+            </center>
+          
+        </div>
+    </div>
+
+   
+  </div>
+</template>
