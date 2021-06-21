a72b6cdf30eb3a602d5577711d5f5ae167c08ced
berny88 <bernard.bougeon@gmail.com>
2021-06-21 06:43:53 +0200

    add avatar

--- dep.sh
+++ dep.sh
@@ -1,15 +1,26 @@
 export TARGET=../dist
+export BACKUP=../backup
+#backup img
+cp -rR $TARGET/dbimg $BACKUP/
+
 cd front 
 npm run build 
 
 echo "cd .."
 cd ..
 mkdir $TARGET
+mkdir $BACKUP
 echo "copy *.py"
 cp app.py $TARGET/.
 
 mkdir $TARGET/front
 mkdir $TARGET/back
+#take car to never remove
+mkdir $TARGET/dbimg
+mkdir $BACKUP/dbimg
+#restore img
+cp -rR $BACKUP/dbimg $TARGET/.
+cp front/src/assets/img/avatar/default_avatar.png $TARGET/dbimg/.
 
 cp front/*.py $TARGET/front/.
 cp -rR back $TARGET/.
